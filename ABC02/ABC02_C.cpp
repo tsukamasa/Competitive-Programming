5445207897a4621317db48e45c47db6c75f8cad6
tsukamasa <masashi19950226@gmail.com>
2016-05-11 16:30:47 +0900

    C問題正解

--- ABC02/ABC02_C.cpp
+++ ABC02/ABC02_C.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <iomanip>
 #include <math.h>
 
 
@@ -8,15 +8,14 @@ using namespace std;
 int main(){
 	int x1,y1,x2,y2,x3,y3;
 	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
-	
-	double a = fabs(sqrt(pow(x2-x1,2.0)+pow(y2-y1,2.0))); 
-	double b = fabs(sqrt(pow(x2-x3,2.0)+pow(y2-y3,2.0))); 
-	double c = fabs(sqrt(pow(x3-x1,2.0)+pow(y3-y1,2.0))); 
 
-	double s = (a + b + c)/2.0;
-	double T = fabs(s*(s-a)*(s-b)*(s-c));
+	int a = x2-x1;
+	int b = y2-y1;
+	int c = x3-x1;
+	int d = y3-y1;
+	double s = abs((a*d)-(b*c))/2.0;
 
-	cout << T << endl;
+	cout << setprecision(10) << s << endl;
 	return 0;
 
 }
