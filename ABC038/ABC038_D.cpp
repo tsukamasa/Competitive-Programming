68a9f4ab7e847f67e054cf41ceb5cfcb4f9d8227
tsukamasa <masashi19950226@gmail.com>
2016-06-10 11:08:00 +0900

    動くように改変

--- ABC038/ABC038_D.cpp
+++ ABC038/ABC038_D.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main() {
   int N;
-  pair pair[100000];
+  map<int,int> map;
   cin >> N;
 
   for (int i = 0; i < N; i++) {
-
+    int a,b;
+    cin >> a >> b;
+    map.insert(make_pair(a,b));
   }
   return 0;
 }
